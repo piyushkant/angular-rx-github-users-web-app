87945b0099b144a377dfb3fff289f32b6250e5c0
Piyush Kant <pkreaper@gmail.com>
2021-05-10 22:33:49 +0900

    add gihub user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GitHubService} from './github.service';
 
 @Component({
-  selector: 'app-root',
-  template: `
-  `
+    selector: 'app-root',
+    template: `
+        <div *ngIf="isLoading"><i class="fa fa-spinner fa-spin fa-3x"></i></div>
+        <h3>GitHub User Results</h3>
+        <div *ngIf="isLoading"><i class="fa fa-spinner fa-spin fa-3x"></i></div>
+        <div *ngFor="let user of users" class="media">
+            <div class="media"><a href="{{ user.html_url }}"> <img src="{{ user.avatar_url }}" class="mr-4" alt="..." width="64"
+                                                                   height="64"> </a>
+                <div class="media-body"><h5 class="mt-0">{{ user.login }}</h5> Score: {{ user.score }} </div>
+            </div>
+        </div>
+    `,
+    providers: [GitHubService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+    isLoading = true;
 
+    users = [];
+
+    // tslint:disable-next-line:variable-name
+    constructor(private _githubService: GitHubService) {
+    }
+
+    // tslint:disable-next-line:typedef
+    ngOnInit() {
+        this._githubService.getGitHubData('greg').subscribe(data => {
+            this.isLoading = false;
+            this.users = data.items;
+            console.log(data.items);
+        });
+    }
 }
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,20 +1,23 @@
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
 
 export interface GitHubUser {
-  html_url: string;
-  avatar_url: string;
-  login: string;
-  score: string;
+    html_url: string;
+    avatar_url: string;
+    login: string;
+    score: string;
+    items: any;
 }
 
+@Injectable()
 export class GitHubService {
-  // tslint:disable-next-line:variable-name
-  constructor(private _http: HttpClient) {
-  }
+    // tslint:disable-next-line:variable-name
+    constructor(private _http: HttpClient) {
+    }
 
-  // tslint:disable-next-line:variable-name
-  getGitHubData(_searchTerm): Observable<GitHubUser> {
-    return this._http.get<GitHubUser>('https://api.github.com/search/users?q=' + _searchTerm);
-  }
+    // tslint:disable-next-line:variable-name
+    getGitHubData(_searchTerm): Observable<GitHubUser> {
+        return this._http.get<GitHubUser>('https://api.github.com/search/users?q=' + _searchTerm);
+    }
 }
